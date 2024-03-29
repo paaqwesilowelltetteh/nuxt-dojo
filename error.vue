633eb8ce9665f1a83d646cacb5c557a29e713c6d
Enoch Tetteh <124895503+paaqwesilowelltetteh@users.noreply.github.com>
2024-03-29 11:09:33 +0000

    Error Pages

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mt-7 mb-4 max-w-sm mx-auto text-center card">
+        <div v-if="error.statusCode === 404">
+            <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+            <p>Ooops.</p>
+        </div>
+        <h1 v-else>An error occurred</h1>
+        <p class="mb-4">{{ error.message }}</p>
+        <button class="btn my-7" v-on:click="handdleClearError">Go to home</button>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    error: Object
+})
+
+const handdleClearError = () => clearError({redirect: '/'});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -13,6 +13,9 @@
                     <li>
                         <NuxtLink to="/products" class="btn">Products</NuxtLink>
                     </li>
+                    <li>
+                        <NuxtLink to="/products/abc" class="btn">Product Abc</NuxtLink>
+                    </li>
                 </ul>
             </nav>
         </header>
